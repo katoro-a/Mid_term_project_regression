2c125d5beddc6b13b2ed617ed0c5e956c4ddefec
katoro-a <katherinetolton@rocketmail.com>
2021-12-04 12:09:51 -0500

    first commit

---
+++ SQL_questions_regression_proj.sql
@@ -0,0 +1,23 @@
+
+
+
+-- Question 4: Select all the data from table house_price_data to check if the data was imported correctly
+select * 
+from house_price_regression;
+-- Question 5:
+-- Question 6:
+-- Question 7: 
+	-- part 1  
+select distinct (bedrooms)
+from house_price_regression;
+	-- part 2
+	-- part 3 
+	-- part 4 
+	-- part 5 
+	-- part 1  
+
+
+
+-- Question 8:
+-- Question 9:
+
